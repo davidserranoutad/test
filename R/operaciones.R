0d6f9b1e97b82d05f0b172d7a3b0f7b5e31a659a
davidserranoutad <david.serrano@live.u-tad.com>
2017-01-05 15:40:17 +0100

    test 1

---
+++ R/operaciones.R
@@ -0,0 +1,31 @@
+#' Suma dos números
+#'
+#' Recibe dos números y devuelve su suma
+#' @param x Primer valor numérico a sumar
+#' @param y Primer valor numérico a sumar
+#' @return La suma de las dos entradas
+#' @examples suma.dos.numeros(1,0)
+#' suma.dos.numeros(-5,3)
+#' @export
+#'
+suma.dos.numeros <- function(x,y){
+
+  return(x+y)
+
+}
+
+#' Multiplica dos números
+#'
+#' Recibe dos números y devuelve su producto
+#' @param x Primer valor numérico a multiplicar
+#' @param y Primer valor numérico a multiplicar
+#' @return El producto de las dos entradas
+#' @examples multiplica.dos.numeros(1,0)
+#' multiplica.dos.numeros(-5,3)
+#' @export
+#'
+multiplica.dos.numeros <- function(x,y){
+
+  return(x*y)
+
+}
